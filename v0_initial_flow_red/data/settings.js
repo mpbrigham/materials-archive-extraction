65cc64696e87bc1d8419fc311679da5daf910385
mpbrigham <marco@brigham.be>
2025-07-24 09:34:46 -0100

    refactoring

---
+++ v0_initial_flow_red/data/settings.js
@@ -0,0 +1,40 @@
+module.exports = {
+    // Flow file
+    flowFile: 'red.json',
+    
+    // User directory
+    userDir: '/data/',
+    
+    // Node-RED scans the `nodes` directory in the userDir to find nodes
+    nodesDir: '/data/nodes',
+    
+    // Logging
+    logging: {
+        console: {
+            level: "info",
+            metrics: false,
+            audit: false
+        }
+    },
+    
+    // Disable editor to run in headless mode (optional)
+    // editorEnabled: false,
+    
+    // Function node settings
+    functionGlobalContext: {
+        // Add any global modules here
+    },
+    
+    // Allow function nodes to load external modules
+    functionExternalModules: true,
+    
+    // Debug max message length
+    debugMaxLength: 10000,
+    
+    // HTTP settings
+    httpAdminRoot: '/',
+    httpNodeRoot: '/',
+    
+    // Security
+    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET
+}
